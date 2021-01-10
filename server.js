5355bf71423b347813f1916e35a2e5d4bb7237ba
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-10 10:31:01 -0800

    pray for te best heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import config from './config.js'
 import app from './src/app.js'
 import * as db from './src/mongo.js'
 
-app(db).listen(config.PORT ?? 8080, () => {
+app(db).listen($PORT ?? 8080, () => {
   config.PORT
     ? console.log(`Live @ http://localhost:${config.PORT}`)
     : console.log('Live @ http://localhost:8080')
