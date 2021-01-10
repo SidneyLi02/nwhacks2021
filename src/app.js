5b6aee22a96f0a26bd143347a918909db4ff1b03
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-10 10:48:09 -0800

    god pls help

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(cookieSession({
 app.use(helmet())
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 export default function (db) {
   app.use('/auth', userRoute())
