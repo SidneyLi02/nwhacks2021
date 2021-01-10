9c88fda46c5316739128d3dc514c3f1daa5eadad
Don <donli90@hotmail.com>
2021-01-10 07:38:44 -0800

    get posts

--- back/src/components/post/postRoute.js
+++ back/src/components/post/postRoute.js
@@ -7,5 +7,6 @@ const router = express.Router()
 
 export default function () {
   router.post('/new', multer.array('images', config.BATCH_IMAGE_UPLOAD_COUNT), postController.newPost)
+  router.get('/get', postController.getPosts)
   return router
 }
