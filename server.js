30636f84e715c7b7f3da7808f73e7ec134610776
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-10 10:37:14 -0800

    pray to god

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import config from './config.js'
 import app from './src/app.js'
 import * as db from './src/mongo.js'
 
-app(db).listen($PORT ?? 8080, () => {
+app(db).listen(process.env.PORT ?? 80, () => {
   config.PORT
     ? console.log(`Live @ http://localhost:${config.PORT}`)
     : console.log('Live @ http://localhost:8080')
