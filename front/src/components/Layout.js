3d89fecdebdfe4e118b334dbb9d7db9ce5c34d7d
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-09 21:27:29 -0800

    carousel done

--- front/src/components/Layout.js
+++ front/src/components/Layout.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Main from './Main';
 import Auth from './Auth';
+import Navbar from 'react-bootstrap/Navbar';
+import Smile from './img/smile.png';
+import Carousel from 'react-bootstrap/Carousel'
+import SlideOne from './img/kek.jpg';
+import SlideTwo from './img/kek2.jpg';
+import SlideThree from './img/kek3.jpg'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+import './Layout.css';
 
 export default function Layout(props) {
 
@@ -31,11 +42,51 @@ export default function Layout(props) {
     else {
         return (
             <div>
-                <p>App Name</p>
+                <h1 className = "appTitle">The Goodness Repository</h1>
+                <Container>
+                    <Row>
+                        <Col></Col>
+                        <Col>
+                        
+                        <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src= { SlideOne }
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src= { SlideTwo }
+                        alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src= { SlideThree }
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+            </Carousel>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+                
+                <div className = "btnDisplay">
                 <button onClick = {() => hasClicked(true)}>Register</button>
                 <button onClick = {() => startTransfer(true)}>Proceed</button>
+                </div>
+
+
+
             </div>
         )
     }
 
 }
+
+
